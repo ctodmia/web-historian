00089e6bb5e20021f421958466fcd96908a8e363
ctodmia <ctodmia@gmail.com>
2015-10-28 14:56:25 -0700

    serving up our index.html

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -11,6 +11,7 @@ exports.headers = headers = {
 };
 
 exports.serveAssets = function(res, asset, callback) {
+  var body = '<html>' + '</html>' //to be completed. 
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,21 +1,30 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-
-//if (req === 'GET'){
-  //return statusCode = 200;
-  //return index.html
-//}
-
 // console.log('this is a request', req);
 var statusCode = 200;
-if (req === 'GET'){
-  res.writeHead(statusCode, {"Content-Type":"text/html"})
-  // res.write (index.html)
-  res.write('public/index.html');
-  res.end(archive.paths.list);
-  
+
+if( req.method === 'GET'){
+  // console.log( "REQUEST-METHOD", req.method );
+  fs.readFile('public/index.html', function(err, html){
+    if (err){
+      res.writeHead(400);
+      res.end();
+    }
+      res.writeHead(statusCode, {'Content-Type': 'text/html'})
+      res.write(html);  
+      res.end();
+  });
 }
+
+
+//   res.writeHead(statusCode, {"Content-Type":"text/html"})
+//   // res.write (index.html)
+//   res.write('public/index.html');
+//   res.end(archive.paths.list);
+  
+  // console.log(archive.paths.list)
 };
