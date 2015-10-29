84e09f3cfd2c8ba87a760d34c662294b71226230
ctodmia <ctodmia@gmail.com>
2015-10-29 12:27:30 -0700

    sites added to sites.text

--- web/request-handler.js
+++ web/request-handler.js
@@ -18,19 +18,25 @@ fs.readFile(path.join(__dirname, './public/index.html'), function(err, html){
   
 
 // POST
-// listen for post 
-// collect data from input @ index.html
+// listen for post = url  
+// collect data (url) from input @ index.html
 // store data in site.txt 
 
 }else if (req.method === 'POST'){
   var chunk ='';
+  
   req.on('data', function(html){
     chunk += html;
-    console.log('this is chunk', chunk);
-
+  fs.appendFile((path.join(__dirname,'./archives/sites.txt')), chunk, function(err){
+     if(err) throw err;
+    });
+  // res.write(); 
+ 
   })
+
   req.on('end', function(){
-    console.log('this is ended');
+    res.writeHead(200);
+    res.redirect((path.join(__dirname, './public/loading.html')));
   })
 }
 
